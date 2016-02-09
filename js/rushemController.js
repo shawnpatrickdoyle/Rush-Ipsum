48c78f900f3bd66b2c7ca3512dd274460a2b57b6
Shawn Patrick Doyle <shawnpatrickdoyle@gmail.com>
2016-02-09 12:47:13 -0500

    Logic for creating array variable set up. Need to figure out triggering selections and inserting text.

--- js/rushemController.js
+++ js/rushemController.js
@@ -1,27 +1,6 @@
-app.controller('rushemController',function($scope, $http){
-	$http.get("lyric.json")
-	.then(function(response){
-		var lyrics = response.data,
-			shuffled = []
-			firstParagraph = [],
-			secondParagraph = [],
-			thirdParagraph = [],
-			fourthParagraph = [],
-			fifthParagraph = [],
-			sixthParagraph = [],
-			seventhParagraph = [],
-			eigthParagraph = [],
-			ninthParagraph = [],
-			tenthParagraph = [],
-			eleventhParagraph = [],
-			twelfthParagraph = [],
-			thirteenthParagraph = [],
-			fourteenthParagraph = [],
-			fifteenthParagraph = [],
-			sixteenthParagraph = [],
-
-	});
-});
+/* Shuffler function randomizes order of input array. 
+@param {array} array - Array to be shuffled.
+*/
 function shuffler (array) {
 	var shuffle = [];
     for(var i=array.length;i>0;i--){
@@ -29,5 +8,38 @@ function shuffler (array) {
         var nextLine = array.splice(rand, 1);
         shuffle.push(nextLine[0]);
     };
-    return shuffle
+    return shuffle;
+};
+/* passageCreator takes the shuffled array and returns an array with
+the desired number of paragraphs of the desired length.
+@param {array} shuffledArray - JSON array that has been shuffled in random order.
+@param {number} passageLength - desired number of paragraphs 
+@param {number} paragraphLength - desired number of sentences in each paragraph
+*/
+
+function passageCreator(shuffledArray, passageLength, paragraphLength){
+	var passage = [];	
+	for (var i=0;i<passageLength;i++){
+        var paragraph = "";
+		for (var j=0;j<paragraphLength;j++){
+			if (j<(paragraphLength-1)){
+				paragraph += shuffledArray.splice(0,1) + " ";
+			} else {
+				paragraph += shuffledArray.splice(0,1);
+			}
+		}
+        shuffledArray = shuffledArray.splice((paragraphLength-1));
+		passage.push(paragraph);
+	}
+	return passage;
 }
+
+app.controller('rushemController',function($scope, $http){
+	$http.get("lyric.json")
+	.then(function(response){
+		var lyrics = response.data,
+			ipsum = shuffler(lyrics),
+		return ipsum	
+	});
+	$scope.passage = passageCreator(ipsum)
+});
