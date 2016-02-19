79fc635801f010cd1d59a29c734f122dc94cca38
Shawn Patrick Doyle <shawnpatrickdoyle@gmail.com>
2016-02-19 18:13:00 -0500

    footer set up

--- js/rushemController.js
+++ js/rushemController.js
@@ -1,3 +1,5 @@
+
+
 /* Shuffler function randomizes order of input array. 
 @param {array} array - Array to be shuffled.
 */
@@ -36,12 +38,13 @@ function passageCreator(shuffledArray, passageLength, paragraphLength){
 }
 
 var effects = function(){
-	jQuery("#starman").hide();
-	jQuery(".lyrics").addClass("well well-lg");
+	jQuery(".lyrics").show();
+	jQuery("footer").hide();
+	jQuery(".replaceFooter").show();
 	var ipsumClone = (JSON.parse(JSON.stringify(ipsum)));
 	var shuffled = shuffler(ipsumClone);
-	return shuffled;
-}
+    return shuffled;
+};
 
 var app = angular.module("rushemIpsum",["ngRoute"]);
 app.controller('rushemController', function($scope){
@@ -61,4 +64,42 @@ app.controller('rushemController', function($scope){
 		var shuffled = effects();
 		$scope.lyrics = passageCreator(shuffled, 16, 10)
 	};
+});
+
+var clipboard = new Clipboard('.copy');
+	clipboard.on('success', function(e) {
+        console.log(e)
+    });
+    clipboard.on('error', function(e) {
+        console.log("error");
+    });
+
+var title = jQuery("#title"),
+	header = jQuery('header'),
+	subtitle = jQuery('#subtitle'),
+	footer = jQuery('footer')
+	article = jQuery('article');
+jQuery(window).scroll(function() {
+	var position = jQuery(document).scrollTop();
+	console.log(position);
+  	if (position > 228) {
+	    header.removeClass("normal").addClass("scroll");
+	    title.removeClass("title").addClass("title-scroll");
+	    subtitle.removeClass("subtitle").addClass("subtitle-scroll");
+	    article.addClass("scroll");
+	    footer.addClass("scroll");
+	    jQuery('h3').addClass("scroll");
+  	}
+});
+jQuery(window).scroll(function() {
+	var position2 = jQuery(document).scrollTop();
+	if (article.hasClass("scroll") && position2 < 229){
+		if (position2 > 0) {
+			console.log ("position2:" + position2);
+			article.css("top", position2+72)
+			}
+		 else {
+		article.removeClass("scroll").addClass("scrollLock");
+		};
+	}
 });
